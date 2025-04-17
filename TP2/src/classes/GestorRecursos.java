8b5109eed4007efd8babc9a659b3ad12ded8a894
Martin-NT <martinnavarro1503@gmail.com>
2025-04-17 00:54:20 -0300

    ArrayList en GestorRecursos

--- TP2/src/classes/GestorRecursos.java
+++ TP2/src/classes/GestorRecursos.java
@@ -1,8 +1,27 @@
 package classes;
-
+import java.util.ArrayList;
+import java.util.List;
+import interfaces.RecursoDigital;
 /**
  * Clase responsable de gestionar los recursos digitales.
  */
-
 public class GestorRecursos {
+    // Lista para almacenar los recursos digitales
+    private List<RecursoDigital> recursos = new ArrayList<>();
+
+    // Constructor que inicializa la lista de recursos
+    public GestorRecursos() {
+        this.recursos = new ArrayList<>();
+    }
+
+    // Agrega un recurso a la lista
+    public void agregarRecurso(RecursoDigital recurso) {
+        recursos.add(recurso);
+    }
+
+    // Getter: Devuelve la lista completa de recursos
+    public List<RecursoDigital> getRecursos() {
+        return recursos;
+    }
+
 }
